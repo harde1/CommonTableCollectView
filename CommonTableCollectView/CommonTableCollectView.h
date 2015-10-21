f39d8b8583b3b7498c78f340cab34144aa43eef6
剑仁不败 <harde1@163.com>
2015-10-21 10:28:50 +0800

    增加了一个头
    增加了一个头

---
+++ CommonTableCollectView/CommonTableCollectView.h
@@ -0,0 +1,17 @@
+//
+//  CommonTableCollectView.h
+//  Pods
+//
+//  Created by 悟空设计部 on 15/10/21.
+//
+//
+
+#ifndef CommonTableCollectView_h
+#define CommonTableCollectView_h
+
+#import "CommonTableView.h"
+#import "CommonCollectionView.h"
+#import "CommonCell.h"
+#import "CommonCollectionCell.h"
+
+#endif /* CommonTableCollectView_h */
